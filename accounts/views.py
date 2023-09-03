c66601d93f7455195c58edd3653ff9be687a037c
shang-sin <xiongxisentian@gmail.com>
2023-09-03 09:26:40 +0900

    loginviewの修正

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
@@ -19,6 +20,10 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class LoginView(LoginView):
+    template_name = "accounts/login.html"
 
 
 class UserProfileView(TemplateView):
