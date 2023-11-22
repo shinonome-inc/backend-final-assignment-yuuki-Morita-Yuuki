87fc5f3569fa3af2a804ced3f9366ff9d51a5a3f
shang-sin <xiongxisentian@gmail.com>
2023-11-22 20:26:38 +0900

    修正1

--- static/js/like.js
+++ static/js/like.js
@@ -11,6 +11,33 @@ const getCookie = (name) => {
 const csrftoken = getCookie('csrftoken');
 
 
+function toggleLike(tweetId) {
+    var likeButton = $("#" + tweetId);
+    var likeCountSpan = $("#likes_count_" + tweetId);
+    var likeUrl = likeButton.data("url");
+    $.ajax({
+        type: "POST",
+        url: likeUrl,
+        dataType: "json",
+        success: function (response) {
+
+
+            likeCountSpan.text(response.likes_count + " いいね数");
+
+            if (likeButton.text() === "いいね") {
+                likeButton.text("いいね取り消し");
+                likeButton.data("url", likeUrl.replace('/like/', '/unlike/'));
+            } else {
+                likeButton.text("いいね");
+                likeButton.data("url", likeUrl.replace('/unlike/', '/like/'));
+            }
+        },
+        error: function (error) {
+            console.log(error);
+            console.log("Ajax error:", error);
+        }
+    });
+}
 
 function toggleLike(tweetId) {
     var likeButton = $("#" + tweetId);
@@ -21,9 +48,9 @@ function toggleLike(tweetId) {
         url: likeUrl,
         dataType: "json",
         success: function (response) {
-            if (response.likes_count !== undefined) {
-                likeCountSpan.text(response.likes_count + " いいね数");
-            }
+
+            likeCountSpan.text(response.likes_count + " いいね数");
+
             if (likeButton.text() === "いいね") {
                 likeButton.text("いいね取り消し");
                 likeButton.data("url", likeUrl.replace('/like/', '/unlike/'));
